001fc07795b50a1249c5a355793d04ae74faaef6
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-11 17:09:50 +0300

    Init

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = '5336424335:AAGk0uyo0qqRCrKgvr2J7GrYKK1S0MF8878';
-const webAppUrl = 'https://ornate-selkie-c27577.netlify.app';
+const token = '5833515258:AAHm5gAkLr7XcD3JGiQdOxDRXky3sby6dUw';
+const webAppUrl = 'https://fluffy-boba-0e3044.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
